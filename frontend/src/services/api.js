28e5f799665ae74318b35f04f1aae81318f76c84
princeptl-work <princebpatel.work@gmail.com>
2026-06-06 18:11:33 +0530

    Update API base URL to Deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api', headers: { 'Content-Type': 'application/json' } });
+const api = axios.create({ baseURL: 'https://vendorbridge-frontend-pi.vercel.app/api', headers: { 'Content-Type': 'application/json' } });
 
 api.interceptors.request.use(
   config => { const token = localStorage.getItem('vb_token'); if (token) config.headers['Authorization'] = `Bearer ${token}`; return config; },
